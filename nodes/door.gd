7a4e8c6784ed6b02398aa9f7b64b666b82646ac8
asus <joaocabralpv@gmail.com>
2024-12-14 11:40:17 -0100

    Removed unused state from Door.State

--- nodes/door.gd
+++ nodes/door.gd
@@ -2,8 +2,8 @@ extends StaticBody2D
 
 class_name Door
 
-enum State{Open,Close,Stop}
-var state:State = State.Stop
+enum State{Open,Close}
+var state:State = State.Close
 
 @export var speed = 3
 
@@ -13,22 +13,9 @@ func open():
 func close():
 	state=State.Close
 
-func _on_button_presed():
-	open()
-
-func _on_button_released():
-	close()
 
 func _process(delta):
 	if state == State.Open:
 		scale.y = clamp(scale.y-(speed*delta),0,1)
 	elif state == State.Close:
 		scale.y = clamp(scale.y+(speed*delta),0,1)
-
-
-func _on_button_body_entered(body):
-	pass # Replace with function body.
-
-
-func _on_button_body_exited(body):
-	pass # Replace with function body.
